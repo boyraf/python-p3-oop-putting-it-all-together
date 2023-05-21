cd39460efc0c44144902a580206a1575506beb83
boyraf <raphael.kymotho@gmail.com>
2023-05-21 20:27:45 +0300

    commit

--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,13 @@
 #!/usr/bin/env python3
 
 class Shoe:
+    def __init__(self, brand, size):
+        self.brand = brand
+        self.size = size
+        self.condition = "Used"
+
+    def cobble(self):
+        print("Your shoe is as good as new!")
+        self.condition = "New"
+
     pass
